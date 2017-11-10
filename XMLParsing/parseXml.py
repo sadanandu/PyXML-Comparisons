d7c01d7104450b3b0d4613976559b454881c91e7
Sadanand Upase <svupase@gmail.com>
2017-11-10 23:13:01 +0530

    Prints file size , start time and end time of the processing

--- XMLParsing/parseXml.py
+++ XMLParsing/parseXml.py
@@ -2,11 +2,16 @@ xml_file= "C:\\Machine Learning\\PyPractice\\sample.xml"
 
 import employee
 import xml.etree.ElementTree as ET
+import time
+import os
 employees = []
+print("FileSize" + str(os.stat(xml_file).st_size))
+print("StartTime" + str(time.time()))
 for event, node in ET.iterparse(xml_file):
     if event.title() == 'End' and node.tag == "EMPLOYEE":
         emp = str(ET.tostring(node))
         employees.append(employee.EmployeeType(emp))
+print("EndTime" + str(time.time()))
 '''
 import pyxb.binding.saxer
 import io
@@ -23,6 +28,9 @@ print(1)
 instance = handler.rootObject()'''
 #x = employee.CreateFromDocument(xml_file)
 #print(x)
+
+
+    
 
 
     
