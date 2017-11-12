4c6803c56ad8458b95bd93e317e4a653403f815a
Sadanand Upase <svupase@gmail.com>
2017-11-12 21:53:14 +0530

    Parses employee objects
    Earlier code was wrong, employee.EmployeeType(text) used to assign all text to EMPID and that was wrong. Now separate binding module for employee will create employee objects. Also added code for sqlite db creation and record insertion.

--- XMLParsing/parseXml.py
+++ XMLParsing/parseXml.py
@@ -1,36 +1,72 @@
-xml_file= "C:\\Machine Learning\\PyPractice\\sample.xml"
+from __future__ import unicode_literals
 
-import employee
+import employee, employeedb
 import xml.etree.ElementTree as ET
+import xml.sax.expatreader
 import time
 import os
-employees = []
-print("FileSize" + str(os.stat(xml_file).st_size))
-print("StartTime" + str(time.time()))
-for event, node in ET.iterparse(xml_file):
-    if event.title() == 'End' and node.tag == "EMPLOYEE":
-        emp = str(ET.tostring(node))
-        employees.append(employee.EmployeeType(emp))
-print("EndTime" + str(time.time()))
-'''
 import pyxb.binding.saxer
-import io
-
-saxer = pyxb.binding.saxer.make_parser()
-print(saxer)
-handler = saxer.getContentHandler()
-print(handler)
-xmlt = open(xml_file, "r").read()
-saxer.parse(io.StringIO(xmlt))
-print(saxer)
-print(handler)
-print(1)
-instance = handler.rootObject()'''
-#x = employee.CreateFromDocument(xml_file)
-#print(x)
+from pyxb.utils import saxutils
+import sqlite3
 
 
+xml_file= "C:\\Machine Learning\\PyPractice\\e1.xml"
+
+def parse_xml_to_get_employee_objets():
+    employees = []
+    print("FileSize" + str(os.stat(xml_file).st_size))
+    print("StartTime" + str(time.time()))
+    for event, node in ET.iterparse(xml_file):
+        if event.title() == 'End' and node.tag == "EMPLOYEE":
+            emp = ET.tostring(node) #This returns a byte string
+            #Decoding bytes stream before sending it for parsing using pyxb module
+            #CreateFromDocument() will create a binding for any XML fragment that is a top-level element in the schema.
+            #Hence changed the schema to have EMPLOYEE as top
+            #level schema in another file and used employee module to parse individual 
+            #employee tags.
+            employees.append(employee.CreateFromDocument(emp.decode('UTF-8'))) 
+    print("EndTime" + str(time.time()))
+    print(employees)
+    return employees
+
+def get_connection():
+    return sqlite3.connect("employee.db")
+
+def create_database(conn):
+    query = "CREATE TABLE EMPLOYEE (emp_id INTEGER, salary INTEGER, dept_id INTEGER)"
+    execute_query(conn ,query)
     
+def execute_query(conn, query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    conn.commit()
 
+def get_records_generator(records):
+    for record in records:
+        yield (record.EMPID, record.SALARY, record.DEPT_ID)
+
+def insert_records_into_db(records):
+    conn = get_connection()
+    insert_query = "INSERT INTO EMPLOYEE values(?, ?, ?)"
+    cursor =conn.cursor()
+    cursor.executemany(insert_query, get_records_generator(records))
+    conn.commit()
+
+'''
+conn = get_connection()
+create_database(conn)
+insert_records_into_db(records)'''
+try:
+    employees = parse_xml_to_get_employee_objets()
+except Exception as e:
+    print(e)
+
+'''Below code will parse the whole document
+print("Now from document")
+xmlt = open(xml_file, "r").read()
+print(type(xmlt))
+print(xmlt)
+x = employee.CreateFromDocument(xmlt)
+print(x)'''
 
     
